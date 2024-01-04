b6ca5c40b047077ec5f151128a5bdd22bee17a6b
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-01-04 16:48:19 +0100

    Create simple side bar with functional component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import FunctionalGreetingComponent from './components/FunctionalGreetingComponent';
-import StatefulGreetingComponenet from './components/StatefulGreetingComponenet';
-import HooksComponent from './components/HooksComponent';
+import "./App.module.css";
+// import FunctionalGreetingComponent from './components/FunctionalGreetingComponent';
+// import StatefulGreetingComponenet from './components/StatefulGreetingComponenet';
+// import HooksComponent from './components/HooksComponent';
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingComponent name="John" />
+      {/* <FunctionalGreetingComponent name="John" />
       <hr />
       <StatefulGreetingComponenet name="Anna" />
       <hr />
-      <HooksComponent name="Bob" />
+      <HooksComponent name="Bob" /> */}
+      <SideBar />
     </div>
   );
 }
